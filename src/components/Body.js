71b0a3a64e2a2df58cbe0ffbf7b33bb12f0a0c02
Prakashreddy000 <prakashreddymunigala22@gmail.com>
2023-11-08 17:27:32 +0530

    first commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,23 +1,27 @@
-
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
 import Header from "./Header";
 import Login from "./Login";
+import Browse from "./Browse";
 
 const Body =()=>{
-        const appRouter = createBrowserRouter([
-            {
-                path:"/",
-                element:<Login/>,
-            },
-            {
-                path:"/header",
-                element:<Header/>,
-            },
+   const appRouter = createBrowserRouter([
+    {
+        path:"/",
+        element:<Login/>,
+    },
+    {
+        path:"/header",
+        element:<Header/>,
+    },
+    {
+        path:"/browse",
+        element:<Browse/>,
+    },
 
-        ])
-
+   ])    
+   
     return(
-        <RouterProvider router={appRouter}/>
+        <RouterProvider router={appRouter}/>  
     );
 };
 export default Body;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,21 @@
-const Login =()=>{
+import Header from "./Header";
+
+const Login = () => {
     return(
-        <div>login</div>
-    )
+        <div>
+            <Header />
+            <div className="absolute">
+            <img src="https://assets.nflxext.com/ffe/siteui/vlv3/ab4b0b22-2ddf-4d48-ae88-c201ae0267e2/0efe6360-4f6d-4b10-beb6-81e0762cfe81/IN-en-20231030-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+            alt="logo"
+            />
+        </div>
+        <form className="relative p-12 bg-black">
+            <input type="text" placeholder="email address" className="p-2 m-2"/>
+            <input type="password" placeholder="password" className="p-2 m-2" />
+            <button className="p-2 m-2">Sign in</button>
+        </form>
+        </div>
+    );
+
 };
 export default Login;
